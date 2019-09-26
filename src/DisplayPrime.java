30555a6747d22ef45236113cbdd0b29896a9a1f3
phamngochau1506@gmail.com <haupham156>
2019-09-26 13:44:35 +0700

    isPrimeNumber

---
+++ src/DisplayPrime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DisplayPrime {
+
+    public static boolean isPrimeNumber(int number) {
+        boolean check = false;
+        int i = 2;
+        while (i < Math.sqrt(number)){
+            if(number % i == 0){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int number =  sc.nextInt();
+        System.out.println("All Prime less than: \n");
+
+        for (int i = 2 ; i < number ; i++){
+            if(isPrimeNumber(i)){
+                System.out.println(" " + i);
+            }
+        }
+    }
+}
